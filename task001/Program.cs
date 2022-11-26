bb0e7af6ec60779d4c04993bdd63c18293bdfbfc
alesiadubik <alek80d@gmail.com>
2022-11-26 17:06:35 +0300

    Add two numbers

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine ());
+int b = Convert.ToInt32(Console.ReadLine ());
